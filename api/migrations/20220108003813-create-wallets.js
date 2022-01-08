fb7f6f54581e388477015fb0a11b7f0765bdc7c5
unknown <carolinne.silva888@gmail.com>
2022-01-07 21:41:53 -0300

    <feat>: <Criação da tabela Wallets>
    <Criação da tabela Wallets e do arquivo de migração>

---
+++ api/migrations/20220108003813-create-wallets.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Wallets', {
+      address: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+      },
+      birthdate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      coin: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Wallets');
+  }
+};

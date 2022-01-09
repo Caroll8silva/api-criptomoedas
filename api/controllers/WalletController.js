4aa39ecddf209e3f4355f86bcb0635834e589475
unknown <carolinne.silva888@gmail.com>
2022-01-09 20:04:38 -0300

    <feat>: <Adicionando a rota delete>
    <Adicionando a rota para deletar uma wallet>

--- api/controllers/WalletController.js
+++ api/controllers/WalletController.js
@@ -1,74 +1,128 @@
 const database = require('../models')
 
 class WalletController {
-     static async pegaTodasAsWallets(req, res){
-        try { 
-
-            const todasAsWallets = await database.Wallets.findAll( { 
-                include: 
-                [{ model: database.Coins, attributes: ['coin','fullname','amont'],
-                as: 'Coins',
-                include: 
-                [{ model: database.Transactions, attributes: ['value','datetime','sendTo','receiveFrom', 'currentCotation'],
-                    as: 'Transactions' }]
-            } ]})
-            
-        return res.status(200).json(todasAsWallets) 
-    
-    }   catch (error) {
-
-        return res.status(500).json(error.message)
-    }
-}
-      static async pegaUmaWallet(req, res){
-         const { address } = req.query
-         try {
-
-       const umaWallet = await database.Wallets.findOne({where: { address: Number(address)}})
+   
+    static async pegaTodasAsWallets(req, res) {
         
-        return res.status(200).json(umaWallet) 
+        try {
 
- }       catch (error) {
+            const todasAsWallets = await database.Wallets.findAll({
+               
+                include: [{
+                  
+                    model: database.Coins,
+                   
+                    attributes: ['coin', 'fullname', 'amont'],
+                    
+                    as: 'Coins',
+                   
+                    include: [{
+                        
+                        model: database.Transactions,
+                       
+                        attributes: ['value', 'datetime', 'sendTo', 'receiveFrom', 'currentCotation'],
+                        
+                        as: 'Transactions'
+                    }]
+                }]
+            })
 
-         return res.status(500).json(error.message)
- 
- }
+            return res.status(200).json(todasAsWallets)
 
-    
- }
+        } catch (error) {
+
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async pegaUmaWallet(req, res) {
+        
+        const { address } = req.params
+
+        try {
+
+            const umaWallet = await database.Wallets.findOne({
+                where: {
+
+                    address: Number(address)
+                }
+            })
+
+            return res.status(200).json(umaWallet)
+
+        } catch (error) {
+
+            return res.status(500).json(error.message)
+
+        }
+
+    }
 
     static async criaWallet(req, res) {
-    const novaWallet = req.body
-    try {
         
-        const novaWalletCriada = await database.Wallets.create(novaWallet, {})
-        
-        return res.status(201).json(novaWalletCriada)
-        
-    } catch (error) {
+        const novaWallet = req.body
+       
+        try {
 
-        return res.status(500).json(error.message)
+            const novaWalletCriada = await database.Wallets.create(novaWallet, {})
+
+            return res.status(201).json(novaWalletCriada)
+
+        } catch (error) {
+
+            return res.status(500).json(error.message)
+        }
     }
-}
+    
     static async atualizaWallet(req, res) {
 
-    const { coin } = req.params
-    const novasInfos = req.body
+        const { coin } = req.params
 
-    try {
+        const novasInfos = req.body
 
-        await database.Wallets.update(novasInfos, { where: { coin : Number(coin)}})
-        const walletAtualizada = await database.Wallets.findOne({  where: { coin: Number(coin)}})
+        try {
+
+            await database.Wallets.update(novasInfos, {
+               
+                where: {
+                    
+                    coin: Number(coin)
+                }
+            })
+            
+            const walletAtualizada = await database.Wallets.findOne({
+                
+                where: {
+                    coin: Number(coin)
+                }
+            })
 
             return res.status(200).json(walletAtualizada)
 
-    } catch (error) {
+        } catch (error) {
 
-        return res.status(500).json(error.message)
+            return res.status(500).json(error.message)
+        }
+
     }
+    static async apagaWallet(req, res) {
 
+        const { address } = req.params
+        
+        try {
+             await database.Wallets.destroy({ where: {
+
+                address: Number(address)
+            
+            } })
+
+            return res.status(204).json()
+
+        } catch (error) {
+
+            return res.status(404).json(error.message)
+        }
     }
-
 
 
 }
--- api/routers/walletsRoute.js
+++ api/routers/walletsRoute.js
@@ -7,5 +7,7 @@ router.get('/api/v1/wallet', WalletController.pegaTodasAsWallets)
 router.get('/api/v1/wallet/:address', WalletController.pegaUmaWallet)
 router.post('/api/v1/wallet', WalletController.criaWallet)
 router.put('/api/v1/wallet/:coin', WalletController.atualizaWallet)
+router.delete('/api/v1/wallet/:address', WalletController.apagaWallet)
+
 module.exports = router
   
